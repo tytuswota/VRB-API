2c1b25dba9d3e230fff81c60185079fac389b023
tytuswota <45140084+tytuswota@users.noreply.github.com>
2021-06-28 13:10:33 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,11 +5,14 @@ from ClientDevice import ClientDevice
 class Client():
     def __init__(self):
         self.DATABASE_CLIENT_METHOD_TYPE_ID = 4
+        self.SEND_TIME_SYNC_METHOD_TYPE_ID = 5
 
     def sendRequest(self, message, msql):
         print(message)
         if message[0] is self.DATABASE_CLIENT_METHOD_TYPE_ID:
             cldb = ClientDataBase()
             cldb.storeData(data=message, mysql=msql)
+        if message[0] is seself.SEND_TIME_SYNC_METHOD_TYPE_ID:
+            #send post request time sync to vrb site php
         else:
             cld = ClientDevice()
